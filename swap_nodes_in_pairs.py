93dd06636b36a5e32a22213641547bd214f9c7eb
Mark Rattle <mark.rattle@gmail.com>
2023-04-30 14:43:15 -0400

    swap nodes in pairs

--- swap_nodes_in_pairs.py
+++ swap_nodes_in_pairs.py
@@ -1,48 +1,66 @@
-from linkedlist import Node
 from linkedlist import LinkedList
 
 class NodeSwapper():
     def swap_nodes_in_pairs(self, node):
-        # if node == None or node.next == None:
-        #     return node
-        # temp1 = node
-        # temp2 = node.next
- 
-        # return node
-        
         if node == None or node.next == None:
-            return node
-        new_head = node.next
-        print("\niteration start")
-        print(f"node: {node}")
-        print(f"node.next: {node.next}")
-        temp = node.next.next
+            return node # base case
+        temp1 = node.next
+        temp2 = node.next.next
         node.next.next = node
-        print(f"temp: {temp}")
-        node.next = temp
-        print("\nafter swapping")
-        print(f"node: {node}")
-        print(f"node.next: {node.next}")
-        if temp:
-            node.next.next = self.swap_nodes_in_pairs(node)
+        node.next = temp2
+        node = temp1
+        node.next.next = self.swap_nodes_in_pairs(node.next.next) # recursive case
         return node
-ns = NodeSwapper()
 
-ll = LinkedList()
-ll.append(1)
-ll.append(2)
-ll.append(3)
-ll.append(4)
-# ll.append(5)
+### TEST CODE ###
+if __name__ == "__main__":
+        
+    ns = NodeSwapper()
 
-print(ll)
+    ### n items, where n is even ###
+    ll = LinkedList()
+    ll.append(1)
+    ll.append(2)
+    ll.append(3)
+    ll.append(4)
+    ll.append(5)
+    ll.append(6)
+    print(f"original LL: {ll}")
 
-swapped_ll = LinkedList()
-swapped = ns.swap_nodes_in_pairs(ll.head)
-swapped_ll.head = swapped
-print(swapped)
-print(swapped.next)
-print(swapped.next.next)
-print(swapped.next.next.next)
-print(swapped.next.next.next.next)
-# print(swapped_ll)
+    swapped_ll = LinkedList()
+    swapped = ns.swap_nodes_in_pairs(ll.head)
+    swapped_ll.head = swapped
+    print(f"Swapped LL: {swapped_ll}")
+
+    ### n items, where n is odd ###
+    ll = LinkedList()
+    ll.append(1)
+    ll.append(2)
+    ll.append(3)
+    ll.append(4)
+    ll.append(5)
+
+    print(f"original LL: {ll}")
+
+    swapped_ll = LinkedList()
+    swapped = ns.swap_nodes_in_pairs(ll.head)
+    swapped_ll.head = swapped
+    print(f"Swapped LL: {swapped_ll}")
+
+    ### single item list ###
+    ll = LinkedList()
+    ll.append(1)
+    print(f"original LL: {ll}")
+
+    swapped_ll = LinkedList()
+    swapped = ns.swap_nodes_in_pairs(ll.head)
+    swapped_ll.head = swapped
+    print(f"Swapped LL: {swapped_ll}")
+
+    ### empty list ###
+    ll = LinkedList()
+    print(f"original LL: {ll}")
+    swapped_ll = LinkedList()
+    swapped = ns.swap_nodes_in_pairs(ll.head)
+    swapped_ll.head = swapped
+    print(f"Swapped LL: {swapped_ll}")
